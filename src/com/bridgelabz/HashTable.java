5cea434a447b0116be328141cff096e9ee181dae
Kisalay <kisalaykisu@gmail.com>
2022-07-01 20:47:55 +0530

    File has been added

--- src/com/bridgelabz/HashTable.java
+++ src/com/bridgelabz/HashTable.java
@@ -1,7 +1,37 @@
 package com.bridgelabz;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Scanner;
 
 public class HashTable {
+    HashMap<String, Integer> MyMapNode = new HashMap<>();
+    private String[] sentenceSplit;
+
     public static void main(String[] agrs){
-        System.out.println("Welcome to Hash Table Programming");
+        HashTable  frequencyObj = new HashTable ();
+        frequencyObj.splitString();
+        frequencyObj.countWords();
+    }
+
+    public void splitString() {
+        System.out.println("Enter the sentence to check frequency of words : ");
+        Scanner scanner = new Scanner(System.in);
+        String sentence = scanner.nextLine();
+        String line = sentence.toLowerCase(Locale.ROOT);
+        sentenceSplit = line.split(" ");
+    }
+
+    public void countWords() {
+        for (String word : sentenceSplit) {
+            if (MyMapNode.containsKey(word)) {
+                MyMapNode.put(word, MyMapNode.get(word) + 1);
+            } else {
+                MyMapNode.put(word, 1);
+            }
+        }
+        for (Map.Entry<String, Integer> entry : MyMapNode.entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
     }
 }
